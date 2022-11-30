86ed7f79994be554ec7b82206d3cb21ab12f78ec
MD ASIF RAZA <113467015+asifforcode@users.noreply.github.com>
2022-11-30 20:55:43 +0530

    Add files via upload
    in this code , i take a input from user as a post and match with following fix statement

---
+++ python word finder program.py
@@ -0,0 +1,7 @@
+#write a program to find out given post is talking  about harry or not
+
+a=input('Enter your post: ')
+if ("asif" in a):
+    print("it is talking about asif")
+else:
+    print("it is not talking about asif")
